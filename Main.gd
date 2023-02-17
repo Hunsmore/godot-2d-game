fb249cf54fdf6ad24702bdaa72e320a5a0aa4a13
hetianfeng <hetianfeng@meituan.com>
2023-02-17 13:13:17 +0800

    finish

--- Main.gd
+++ Main.gd
@@ -5,19 +5,23 @@ var score
 
 func _ready():
 	randomize()
-	new_game()
 
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
+	get_tree().call_group("mobs", "queue_free")
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 func _on_ScoreTimer_timeout():
-	score += 1 # Replace with function body.
+	score += 1
+	$HUD.update_score(score)
 
 
 func _on_StartTimer_timeout():
@@ -26,7 +30,6 @@ func _on_StartTimer_timeout():
 
 
 func _on_MobTimer_timeout():
-	print(1)
 	var mob = mob_scene.instance()
 	var mob_spawn_location = get_node("Path2D/PathFollow2D")
 	mob_spawn_location.offset = randi()
